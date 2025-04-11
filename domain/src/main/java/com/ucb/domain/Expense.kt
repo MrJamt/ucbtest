a8fc7588cfb3ead32118fd79e397355c0a8ad8a7
AdrianMontano528 <jadrian.montanot2011@gmail.com>
2025-04-10 20:44:33 -0400

    Se crearon las dos clases Expense e Income

---
+++ domain/src/main/java/com/ucb/domain/Expense.kt
@@ -0,0 +1,8 @@
+package com.ucb.domain
+
+data class Expense(
+    val name: String,
+    val price: Double,
+    val description: String,
+    val date: String,
+)
---
+++ domain/src/main/java/com/ucb/domain/Income.kt
@@ -0,0 +1,8 @@
+package com.ucb.domain
+
+data class Income(
+    val name: String,
+    val price: Double,
+    val description: String,
+    val date: String,
+)
